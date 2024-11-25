743b46abb1f1e8f09d4f898b41cf923ea75f3459
Christian Jimenez <cajt@outlook.com>
2024-11-25 12:17:51 -0800

    Jquery practice done

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 $(function() {
-  // YOUR CODE HERE
+  // your code here
+  // part 1
+  $("#toggleButton").on("click", function(){
+    $("#myParagraph").toggle();
+  });
+  // part 2
+  $("#colorButton").on("click", function(){
+    $("#colorDiv").css("background-color", "yellow");
+  });
+  // part 3
+  $("#addClassButton").on("click", function(){
+    $("#classDiv").addClass("newclass")
+  }); 
+  $("#removeClassButton").on("click", function() {
+    $("#classDiv").removeClass("newclass")
+  });
+  // part 4
+  $("#fadeInButton").on("click", function(){
+    $("#fadeDiv").fadeIn()
+  });
+  $("#fadeOutButton").on("click", function(){
+    $("#fadeDiv").fadeOut()
+  });
+  // part 5
+  $("#slideUpButton").on("click", function(){
+    $("#slideDiv").slideUp()
+  })
+  $("#slideDownButton").on("click", function(){
+    $("#slideDiv").slideDown()
+  })
+
 })
